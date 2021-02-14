55d60d5c0b47bee51ff4bcb1d3ee562ad083800c
kanis kongpod <78584822+kaniskp@users.noreply.github.com>
2021-02-14 22:30:48 +0700

    upload

--- test5.2.py
+++ test5.2.py
@@ -1,3 +1,40 @@
-class store :
-    def __init__(self,namesh,product,rates,amount)
+num = 0 
+products_ =[]
+rates_ = []
+class shop :
+    def __init__(self,products,rates):
+        self.products = products
+        self.rates = rates
+    def showshop(self):
+        print(i+1,'.',self.products,self.rates,' บาท')
+    def show():
+        print('-'*20,'ร้านนายดอม','-'*20)
+        print('\tแสดงรายการสินค้า [a]\n\tเพิ่มรายการสินค้า[s]\n\tออกจากระบบ[x]')
+
+while True :
+    shop.show() 
+    menu = input('กรุณาเลือกคำสั่ง :   ')
+    if menu == 'a':
+        for i in range(num):
+            x = shop(products_[i],rates_[i])
+            x.showshop()
+    if menu == 's' :
+        while True :
+            print('เพิ่มรายการสินค้า หากต้องการยกเลิกให้กรอก exit')
+            g = input('เพิ่มชื่อสินค้า :\t')
+            if  g == 'exit' :
+                break
+            else:
+                products_.append(g) 
+                r = input('ใส่ราคาของ'+g+': ')
+                rates_.append(r)
+                print('-'*5+'[บันทึกข้อมูลสินค้าแล้ว]'+'-'*5)
+                num +=1
+                yn = input('ต้องการเพิ่มสินค้าอีกหรือไม่(y/n): ')
+                if yn == 'n' :
+                    break
+    if menu == 'x' :
+        yn = input('ต้องการออกจากระบบหรือไม่(y/n): ')
+        if yn == 'y' :
+            break
     
---
+++ week6/week6.1.py
@@ -0,0 +1,74 @@
+'''import sqlite3
+def insertTousers (fname,lName,email) :
+    try :
+        conn = sqlite3.connect (r"D:\kanis_python\week6\week6.1.db")
+        c = conn.cursor()
+
+        data = ('A','A','A')
+        c.executemany('INSERT INTO users (fname,lname,email) VALUES (?,?,?)',data)
+        conn.commit ()
+        c.close ()
+
+    except sqlite3.Error as e :
+        print('Failend to insert : ',e)
+    finally :
+
+        if conn :
+            conn.close ()
+
+insertTousers('kanyanat','kongpod','kanyanat@gmail.com')
+insertTousers ('dom','aom','d@gmail.com')
+'''
+
+'''import sqlite3 
+conn = sqlite3.connect (r'D:\kanis_python\week6\week6.1.db')
+c = conn.cursor()
+try :
+    data = ('B','B','B'),('C','C','C'),('D','D','D')
+    c.executemany('INSERT INTO users (fname,lname,email) VALUES (?,?,?)',data)
+    conn.commit ()
+    c.close ()
+except sqlite3.Error as e :
+    print(e)
+
+finally :
+    if conn :
+        conn.close () '''
+
+'''import sqlite3
+
+conn = sqlite3.connect(r"D:\kanis_python\week6\week6.1.db")
+c = conn.cursor()
+
+try : 
+    c.execute('SELECT * FROM users ORDER BY id  ')
+    conn.commit ()
+    result = c.fetchall()
+    for x in result :
+        print(x)
+    c.close()
+
+except sqlite3.Error as e :
+    print(e)
+
+finally :
+    if conn :
+        conn.close () '''
+
+import sqlite3
+conn = sqlite3.connect (r'D:\kanis_python\week6\week6.1.db')
+c = conn.cursor()
+
+try :
+    c.execute(' SELECT * FROM users LIMIT 5 OFFSET 2 ')
+    result = c.fetchall()
+    for x in result :
+        print(x)
+    c.close ()
+    
+except sqlite3.Error as e :
+    print(e)
+    
+finally : 
+    if conn :
+        conn.cloes()
